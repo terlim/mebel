b62913b0699a9e4a88e3f615ffcdf9e6ac27365a
terlim <terlim@yandex.ru>
2018-12-16 18:02:22 +0300

    left menu

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,41 @@
 //= ../../node_modules/jquery/dist/jquery.js
 //= ../../node_modules/popper.js/dist/umd/popper.js
+//= ../../bootstrap-offcanvas/dist/js/bootstrap.offcanvas.js
 //= ../../node_modules/bootstrap/dist/js/bootstrap.min.js
-$(document).ready(function () {
+//= ../../node_modules/swiper/dist/js/swiper.min.js
 
+$(document).ready(function () {
+    var mySwiper = new Swiper ('#swiper-promo', {
+        // Disable preloading of all images
+        preloadImages: false,
+        // Enable lazy loading
+        lazy: {
+            loadPrevNext: true,
+        },
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        grabCursor: true,
+        effect: 'slide',
+        speed: 1000,
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        autoplay: {
+            delay: 4000,
+        },
+
+        // Default parameters
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loadPrevNextAmount: 2,
+
+    });
 });
